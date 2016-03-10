26550455f12682dd45a2f0284bc952947532422e
Felipe Andrada <felipe.andrada@gmail.com>
2016-03-10 17:55:39 -0300

    user authentication

--- app/services/user-data.js
+++ app/services/user-data.js
@@ -1,15 +1,17 @@
 import {Injectable, Inject} from 'angular2/core';
 import {Storage, LocalStorage, Events} from 'ionic-framework/ionic';
-import {Http} from 'angular2/http';
+import {Http, Headers} from 'angular2/http';
+// import 'rxjs/add/operator/map';
 
 @Injectable()
 export class UserData {
+  public headers: Headers;
 
   static get parameters() {
     return [[Http]];
   }
 
-  constructor(http) {
+  constructor(http, headers) {
     this.http = http;
     this.storage = new Storage(LocalStorage);
     this.storage.get('loggedIn').then((value) => {
@@ -22,15 +24,19 @@ export class UserData {
       this.loggedRole = JSON.parse(value);
     });
 
+
+
     this.userData = false;
   }
 
   getLogin(username, password) {
+    var data = "username="+ username +"&password="+ password;
 
-    var data = {username: username, password: password}
+    this.headers = new Headers();
+    this.headers.append('Content-Type', 'application/x-www-form-urlencoded');
 
     return new Promise(resolve => {
-      this.http.post('http://feiraup.ngrok.com/user/login/', data).subscribe(res => {
+      this.http.post('http://feiraup.ngrok.com/user/login', data, {headers: this.headers}).subscribe(res => {
         this.userData = this.processData(res.json());
         this.setCurrentUserData(this.userData);
         resolve(this.userData);
