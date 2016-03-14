94c430bee087118d10de3dab92929446a064c6be
Felipe Andrada <felipe.andrada@gmail.com>
2016-03-14 18:01:26 -0300

    implementing gmaps geolocation

--- app/pages/route/new-route.js
+++ app/pages/route/new-route.js
@@ -1,4 +1,5 @@
 import {Page, Alert} from 'ionic-framework/ionic';
+// import {Xs} from 'plugins/cordova-plugin-geolocation';
 // import {RouteData}
 
 @Page ({
@@ -18,8 +19,49 @@ export class NewRoutePage {
     this.firstPosition = false;
     this.lastPosition = false;
 
-    this.currentLat = -100.0;
-    this.currentLng = 100.0;
+    this.latLng = null;
+    this.currentLat = 0;
+    this.currentLng = 0;
+
+    this.map = null;
+
+    this.loadSdk();
+    this.loadMap();
+  }
+
+  loadSdk() {
+    let script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'http://maps.google.com/maps/api/js?sensor=true';
+    document.body.appendChild(script);
+  }
+
+  loadMap() {
+    let options = {timeout: 10000, enableHightAccuracy: false}
+
+    navigator.geolocation.getCurrentPosition((position) => {
+
+      this.currentLat = position.coords.latitude;
+      this.currentLng = position.coords.longitude;
+
+      console.log(position.coords);
+
+      this.latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    },()=>{}, options);
+    // navigator.geolocation.getCurrentPosition(options).then((position) => {
+      // console.log(position)
+    //   let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    //   let mapOptions = {
+    //     center: latLng,
+    //     zoom: 15,
+    //     mapTypeId: google.maps.MapTypeId.ROADMAP
+    //   }
+
+    //   this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    // });
   }
 
   onStart() {
