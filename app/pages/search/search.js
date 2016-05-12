4dc5d83a9fd664b3fecb43c2513f25f8ef9e9082
Felipe Andrada <felipe.andrada@gmail.com>
2016-05-11 21:08:00 -0300

    implementing search page

--- app/pages/search/search.js
+++ app/pages/search/search.js
@@ -1,10 +1,36 @@
 import {Page} from 'ionic-framework/index';
 
 @Page ({
-  templateUrl: 'build/pages/search/search.html'
+  templateUrl: 'build/pages/search/search.html',
+  styles: [`
+  .icon {
+    position: relative;
+    z-index: 10;
+  }
+  [fab-top] {
+    top: -34px;
+  }
+  .results {
+    margin: 20px 0 50px;
+  }
+  `]
 })
 
 export class SearchPage {
+  searchQuery;
+
   constructor() {
+    this.shops = [
+    {image: 'http://ionicframework.com/dist/preview-app/www/img/badu-live.png'},
+    {image: 'http://ionicframework.com/dist/preview-app/www/img/advance-card-map-mario.png'},
+    {image: 'http://ionicframework.com/dist/preview-app/www/img/card-sf.png'},
+    {image: 'http://ionicframework.com/dist/preview-app/www/img/badu-live.png'},
+    {image: 'http://ionicframework.com/dist/preview-app/www/img/advance-card-map-mario.png'},
+    {image: 'http://ionicframework.com/dist/preview-app/www/img/card-sf.png'},
+    ];
+  }
+
+  onGetShops(searchBar) {
+    alert(searchBar.value);
   }
 }
