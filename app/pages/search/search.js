96d9c33c6e816074c9ca58663529251f984420f5
Felipe Andrada <felipe.andrada@gmail.com>
2016-05-21 02:55:28 -0300

    start implements advanced search

--- app/pages/search/search.js
+++ app/pages/search/search.js
@@ -1,6 +1,7 @@
 import {Page, NavController} from 'ionic-framework/index';
 import {ShopData} from '../../services/shop-data';
 import {ShowShopPage} from '../shop/show-shop';
+import {FieldCategories} from '../../components/field-categories';
 
 @Page ({
   templateUrl: 'build/pages/search/search.html',
@@ -15,7 +16,8 @@ import {ShowShopPage} from '../shop/show-shop';
   .results {
     margin: 20px 0 50px;
   }
-  `]
+  `],
+  directives: [FieldCategories]
 })
 
 export class SearchPage {
@@ -29,6 +31,7 @@ export class SearchPage {
     this.nav = nav;
     this.shopData = shopData;
     this.shops = [];
+    this.isAdvancedSearch = false;
   }
 
   onGetShops(searchBar) {
@@ -45,5 +48,14 @@ export class SearchPage {
 
   onOpenShop(shop) {
     this.nav.push(ShowShopPage, {shop: shop});
+  }
+
+  categoriesChange(selected) {
+    this.selectedCategories = selected;
+    console.log(this.selectedCategories);
+  }
+
+  onAdvancedSearch() {
+    this.isAdvancedSearch = this.isAdvancedSearch ? false : true;
   }
 }
